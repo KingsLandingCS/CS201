2213965d651453993d3cadc5809aee28a996c2fe
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-10-12 08:08:55 +0500

    learning

---
+++ Lec-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()                    // main is the main function, int is the return type
+{                             // squiggly brackets are used to define a block of function, function body
+    cout << "Hello World!\n"; // ';' is a delimiter
+
+    return 0;
+}
+
+// in high level language the code we right, is called as source code
+// then we compile it into machine code / binary,
+// which is called as object code
--- hello-world.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()                  // main is the main function, int is the return type
-{                           // squiggly brackets are used to define a block of function, function body
-    cout << "Hello World!"; // ';' is a delimiter
-
-    return 0;
-}
