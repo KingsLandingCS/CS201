65c7bcf236098ef7c3180fded21ad52fdce2c8bb
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-10-12 08:35:10 +0500

    learning

--- Lec-1.cpp
+++ Lec-1.cpp
@@ -7,7 +7,7 @@ int main() // main is the main function, int is the return type
     int x = 20; // int is a data type, x (name of variable) is a variable, 20 is a value, '=' is an assignment operator
     int c;
     short y;
-    long z;    // variables declaration
+    long z; // variables declaration
     y = 10;
     c = 20;    // When we store a value in a variable the very first time it is known as initialization
     z = x + y; // Arithmetic operation
@@ -25,8 +25,13 @@ int main() // main is the main function, int is the return type
 // int takes 4 bytes of memory   ==> 32 bits
 // short takes 2 bytes of memory ==> 16 bits
 // long takes 8 bytes of memory  ==> 64 bits
-// float takes 4 bytes of memory
+// float takes 4 bytes of memory for real numbers
 // double takes 8 bytes of memory
 // char takes 1 byte of memory
 // bool takes 1 byte of memory
 // string takes 4 bytes of memory
+// we can store -32,768 to 32,767 in short
+// we can store −2,147,483,648 to 2,147,483,647 in int
+// we can store -2,147,483,648 to 2,147,483,647 in long
+// float range -3.4e38 to 3.4e38
+// double range -1.7e308 to 1.7e308
---
+++ output/lec-1-example-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+    int z;
+
+    x = 5;
+    y = 10;
+    z = x + y;
+    cout << "x= ";
+    cout << x << endl;
+    cout << "y=";
+    cout << y << endl;
+    cout << "z = x + y =";
+    cout << z << endl;
+
+    return 0;
+}
