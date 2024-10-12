c79c7ed98708a554fbc894f8a0a18134547e3f32
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-10-12 08:04:23 +0500

    learning

---
+++ hello-world.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()                  // main is the main function, int is the return type
+{                           // squiggly brackets are used to define a block of function, function body
+    cout << "Hello World!"; // ';' is a delimiter
+
+    return 0;
+}
