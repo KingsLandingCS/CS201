cbd6f5f92133a1012b18a9642b6a294f938dd177
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-10-20 22:16:28 +0500

    learning

---
+++ lecture-3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float len;
+    int len1;
+    float wid;
+    float area;
+
+    len = 13.5;
+    wid = 20;
+
+    area = len * wid;
+
+    cout << " Area = " << area << endl;
+}
