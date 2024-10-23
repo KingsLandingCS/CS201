1768acb50e797237f6cedf069e64cb1248cf3bd6
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-10-23 16:53:08 +0500

    learning

---
+++ lec-6-example-1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // a++ post fix increment operator
+    // ++a pre fix increment operator
+    // a-- post fix decrement operator
+    // --a pre fix decrement operator
+
+    while (1) //===> 1 is true so the loop will run infinitely
+    {
+        cout << " Hello World! " << endl;
+    }
+
+    return 0;
+}
+
+// when you store a value in a variable more than its capacity is called overflow
---
+++ lec-6-example-3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // a++ post fix increment operator
+    // ++a pre fix increment operator
+    // a-- post fix decrement operator
+    // --a pre fix decrement operator
+
+    int start = 1;
+    int sum = 0;
+    while (start <= 100) //===> note if start is 1 is true so the loop will run infinitely
+    {
+        if (start % 2 == 1) // ===> odd value
+        {
+            sum += start;
+        }
+        start++;
+
+        cout << "Sum is:" << sum << endl;
+    }
+
+    return 0;
+}
+
+// when you store a value in a variable more than its capacity is called overflow
