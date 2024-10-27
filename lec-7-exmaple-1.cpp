8905cd88a8e2be117cf5e996a5918686d5024923
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-10-28 01:24:14 +0500

    learning

---
+++ lec-7-exmaple-1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    // while loop - first check condition
+    //  do while loop - atleast executes the block qoute once before checking the condition
+    //  for loop - first checks the conditon
+
+    for (int i = 1; i <= 5; i++)
+    {
+        cout << "5 * " << i << " = " << 5 * i << endl;
+    }
+
+    return 0;
+}
