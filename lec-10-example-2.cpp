51672848bf286cfc54a1fa0ae802516b1ef2d48d
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-11-14 21:11:03 +0500

    learning

---
+++ lec-10-example-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+// #include <conio.h>
+using namespace std;
+
+void test()
+{
+    int x = 10;
+    cout << "Value of x : " << x << endl;
+}
+
+int main()
+{
+    test();
+    // getch();
+    return 0;
+}
