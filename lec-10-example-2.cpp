ab59cefd313d3be05c98f7bb7c12ecf0b98feab1
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-11-14 21:22:09 +0500

    learning

--- lec-10-example-2.cpp
+++ lec-10-example-2.cpp
@@ -2,15 +2,21 @@
 // #include <conio.h>
 using namespace std;
 
-void test()
+void test(int a, char b, string c)
 {
     int x = 10;
-    cout << "Value of x : " << x << endl;
-}
+    cout << "Value of x : " << x + a << " " << b << " " << c << endl;
+};
 
 int main()
 {
-    test();
+    // test(10, 'H', "Underwood"); call by Value/Argument
+
+    int i = 10;
+    char d = 'H';
+    string e = "Underwood";
+
+    test(i, d, e); // Call by variable
     // getch();
     return 0;
-}
+};
