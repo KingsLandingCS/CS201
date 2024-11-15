ad38866e32ce27f08d6533278ff1f80791c9836b
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-11-16 03:27:17 +0500

    learning

---
+++ lec-11-example-1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+// #include <conio.h>
+using namespace std;
+
+int main()
+{
+    string arr[5] = {"Hello", "World!", "Underwood", "Woods", "Arboretum"};
+
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "" << arr[i] << endl;
+    }
+
+    // getch();
+    return 0;
+}
