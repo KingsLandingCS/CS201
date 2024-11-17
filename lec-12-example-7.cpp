9c166d392c58c9741967387e526313b062f67904
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-11-17 21:21:20 +0500

    learning

--- lec-12-example-7.cpp
+++ lec-12-example-7.cpp
@@ -10,9 +10,10 @@ int main()
     {
         for (int j = 0; j <= 2; j++)
         {
-            cout << " " << a[i][j] << endl;
+            cout << " " << a[i][j];
         }
-    }
+        }
+    cout << endl;
     // getch();
     return 0;
 }
