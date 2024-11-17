a7073e5328be374088ab7390b67f14aaccfd5f8b
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-11-17 05:30:57 +0500

    learning

---
+++ lec-12.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+// #include <conio.h>
+using namespace std;
+
+int main()
+{
+
+    char array[5] = {'A', 'H', 'I', 'Z'};
+
+    cout << array << endl;
+
+    // getch();
+    return 0;
+}
