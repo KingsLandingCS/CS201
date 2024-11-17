ec95c2fbcc3d88e2dd03641cda0460d8e2365276
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-11-17 06:01:20 +0500

    learning

---
+++ lec-12-example-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+// #include <conio.h>
+using namespace std;
+
+int main()
+{
+    char a[10];
+
+    cout << "Enter your name: ";
+    cin >> a;
+    cout << a << endl;
+    // getch();
+    return 0;
+}
