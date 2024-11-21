13f302a12391072d58b96f59a97f3c2a95d8ac4e
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-11-22 04:33:33 +0500

    learning

--- lec-14-example-2.cpp
+++ lec-14-example-2.cpp
@@ -8,10 +8,10 @@ main()
     int *ptr1, *ptr2;
     ptr1 = &x;
     ptr2 = &y;
-    *ptr1 = *ptr1 + *ptr2;
 
-    *ptr2 = *ptr1 - *ptr2;
-    *ptr1 = *ptr1 - *ptr2;
+    *ptr1 = *ptr1 + *ptr2; // ptr1 = 30
+    *ptr2 = *ptr1 - *ptr2; // ptr2 = 10
+    *ptr1 = *ptr1 - *ptr2; // ptr1 = 20
 
     cout << "Value of x : " << *ptr1 << endl;
     cout << "Value of y : " << *ptr2 << endl;
