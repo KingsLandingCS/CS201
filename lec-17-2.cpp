4c26cfb84efae721019295295d0949d5911c8926
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-12-06 20:04:11 +0500

    learning

---
+++ lec-17-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+// #include <conio.h>
+#include <string.h>
+using namespace std;
+
+main()
+{
+    char str1[] = "Zubi";
+    char str2[] = "Underwood";
+    cout << "Before Copy: ";
+    cout << " " << str1 << "\t" << str2 << endl;
+    strcpy(str1, str2);
+    cout << "After Copy: ";
+    cout << " " << str1 << "\t" << str2 << endl;
+
+    // getch();
+    return 0;
+}
