99a12ae8ad7cdd5f56609383f80241541f5d5104
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-12-06 18:37:35 +0500

    learning

---
+++ lec-17.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+// #include <conio.h>
+
+using namespace std;
+
+main()
+{
+    char ch;
+    for (int i = 0; i <= 255; i++)
+    {
+        ch = i;
+        cout << " " << i << " " << ch << "\t";
+    }
+
+    cout << endl;
+
+    // getch();
+}
