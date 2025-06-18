05bba6f7306f262a933dcae8333cd18aa359717b
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2025-06-18 08:02:34 +0500

    learning

--- lec-19-1.cpp
+++ lec-19-1.cpp
@@ -5,11 +5,18 @@ using namespace std;
 
 int main()
 {
+    string str;
     fstream myFile;
     myFile.open("test.txt", ios::out);
     myFile << "Hello ThornField";
     myFile.close();
     myFile.open("test.txt", ios::in);
+    myFile.seekg(6, ios::beg);
+    long position = myFile.tellg();
+    cout << "Position: " << position << endl;
+    getline(myFile, str);
+    cout << str << endl;
+    myFile.close();
 
     return 0;
 }
