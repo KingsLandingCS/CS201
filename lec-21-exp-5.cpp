94e14f645c911d45b21a8abd0f8f7619c163949b
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2025-06-23 15:22:57 +0500

    learning

---
+++ lec-21-exp-5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 1;
+    // 0001 = 0010
+    cout << "Shift Left:" << (a << 1) << endl;
+    return 0;
+}
---
+++ lec-21-exp-6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 1;
+    // 0001 = 0000
+    cout << "Shift Right: " << (a >> 1) << endl;
+    return 0;
+}
