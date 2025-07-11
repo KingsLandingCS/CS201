6707eae0b7c756d8c333bf870b74675bd5828fe9
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2025-07-11 09:58:57 +0500

    learning

--- lec-30 -2.cpp
+++ lec-30 -2.cpp
@@ -7,8 +7,8 @@ int main()
     // Pointer variable, pointer takes a separate block of memory as opposed to reference that works on the same block of memory, Pointer stores the address of another
     // variable's memory block
     int *ptr = &i;
-    // cout << ptr << endl; ==> this will show the address in the console
+    cout << ptr << endl; // ==> this will show the address in the console
     // cout << *ptr << endl;  ==> and this will show the value that is represent in the memory block also known as Dereferencing
-    cout << &ptr << endl; // ==> &ptr is known as address operator with '&' that shows the address of the pointer in the console
+    // cout << &ptr << endl;  ==> &ptr is known as address operator with '&' that shows the address of the pointer in the console
     return 0;
 }
