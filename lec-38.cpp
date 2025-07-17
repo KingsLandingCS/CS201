382476c24a8b26faf6f81b17dde06889984c5242
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2025-07-17 14:09:16 +0500

    learning

--- lec-38.cpp
+++ lec-38.cpp
@@ -12,9 +12,19 @@ ostream &Tab(ostream &out)
     out << "\t";
     return out;
 };
+ostream &backSpace(ostream &out)
+{
+    out << "\b";
+    return out;
+};
+ostream &Beep(ostream &out)
+{
+    out << "\a";
+    return out;
+};
 
 int main()
 {
-    cout << newLine << Tab << "Zubair UnderWood" << endl;
+    cout << newLine << Tab << backSpace << Beep << "Zubair UnderWood" << endl;
     return 0;
 }
